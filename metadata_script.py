56135f21f036f3d5c74a8836123dc829ea138695
Aditya Kanungo <aditya.kanungo@gmail.com>
2023-10-11 10:46:46 -0400

    Update metadata_script.py

--- metadata_script.py
+++ metadata_script.py
@@ -1,77 +1,33 @@
 import re
 import json
 
-def extract_metadata_from_sql(filename):
-    with open(filename, 'r') as f:
-        content = f.read()
+sql_script = """
+CREATE TABLE sample_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    birthdate DATE
+);
+"""
 
-    metadata = {}
+# Extract table name
+table_match = re.search(r"CREATE TABLE (\w+)", sql_script)
+table_name = table_match.group(1) if table_match else None
 
-    # Regular expression patterns
-    table_pattern = re.compile(r"CREATE TABLE ([^\s(]+)\s*\(([^;]+)\)", re.IGNORECASE | re.DOTALL)
-    column_pattern = re.compile(r"([^\s,]+)\s+([^\s,]+)", re.IGNORECASE)
-    primary_key_pattern = re.compile(r"PRIMARY KEY\s*\(([^\)]+)\)", re.IGNORECASE)
-    unique_pattern = re.compile(r"UNIQUE\s*\(([^\)]+)\)", re.IGNORECASE)
-    foreign_key_pattern = re.compile(r"FOREIGN KEY\s*\(([^\)]+)\)\s*REFERENCES\s*([^\s(]+)\s*\(([^\)]+)\)", re.IGNORECASE)
-    default_pattern = re.compile(r"DEFAULT\s+([^\s,]+)", re.IGNORECASE)
+# Extract columns
+columns = re.findall(r"(\w+) (\w+\(?\d*?\)?),?", sql_script)
 
-    for table_match in table_pattern.findall(content):
-        table_name = table_match[0].strip()
-        columns_content = table_match[1].strip()
-        columns = [col.strip() for col in columns_content.split(',')]
+# Construct the dictionary for JSON output
+table_metadata = {
+    "table_name": table_name,
+    "columns": []
+}
 
-        table_data = {
-            "columns": {},
-            "primary_key": [],
-            "unique": [],
-            "foreign_keys": [],
-            "defaults": {}
-        }
+for col_name, col_type in columns:
+    table_metadata["columns"].append({
+        "column_name": col_name,
+        "column_type": col_type
+    })
 
-        for column in columns:
-            # Columns and data types
-            match = column_pattern.search(column)
-            if match:
-                column_name = match.group(1).strip()
-                column_type = match.group(2).split(' ')[0].strip()  # Simplified type extraction
-                table_data["columns"][column_name] = column_type
-
-                # Default values
-                default_match = default_pattern.search(column)
-                if default_match:
-                    default_value = default_match.group(1).strip()
-                    table_data["defaults"][column_name] = default_value
-
-        # Primary keys
-        pk_match = primary_key_pattern.search(columns_content)
-        if pk_match:
-            keys = pk_match.group(1).strip().split(',')
-            table_data["primary_key"] = [key.strip() for key in keys]
-
-        # Unique constraints
-        unique_match = unique_pattern.search(columns_content)
-        if unique_match:
-            unique_cols = unique_match.group(1).strip().split(',')
-            table_data["unique"] = [col.strip() for col in unique_cols]
-
-        # Foreign keys
-        for fk_match in foreign_key_pattern.findall(columns_content):
-            fk_data = {
-                "columns": [col.strip() for col in fk_match[0].split(',')],
-                "reference_table": fk_match[1].strip(),
-                "reference_columns": [col.strip() for col in fk_match[2].split(',')]
-            }
-            table_data["foreign_keys"].append(fk_data)
-
-        metadata[table_name] = table_data
-
-    return metadata
-
-def save_metadata_to_json(metadata, output_filename):
-    with open(output_filename, 'w') as f:
-        json.dump(metadata, f, indent=4)
-
-# Example usage:
-filename = "path_to_your_sql_file.sql"
-metadata = extract_metadata_from_sql(filename)
-save_metadata_to_json(metadata, "output_metadata.json")
+# Serialize the dictionary to JSON and print
+json_output = json.dumps(table_metadata, indent=4)
+print(json_output)
