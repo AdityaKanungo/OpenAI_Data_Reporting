b00d8409a5965d0de95fab37f2d656f90ed59106
Aditya Kanungo <aditya.kanungo@gmail.com>
2023-10-11 11:06:17 -0400

    Update metadata_script.py

--- metadata_script.py
+++ metadata_script.py
@@ -9,26 +9,34 @@ CREATE TABLE sample_table (
 );
 """
 
-# Extract table name
-table_match = re.search(r"CREATE TABLE \[?(\w+)\]?", sql_script)
-table_name = table_match.group(1) if table_match else None
 
-# Extract columns
-columns = re.findall(r"\[?(\w+)\]?\s+(\w+\s*\(?[\w\s,]*\)?\s*\w*)[,)]", sql_script)
+# Find all CREATE TABLE segments
+create_statements = re.findall(r"CREATE TABLE \[?(\w+)\]? \{(.*?)\} ON \[?\w+\]?", sql_script, re.DOTALL)
 
-# Construct the dictionary for JSON output
-table_metadata = {
-    "table_name": table_name,
-    "columns": []
-}
+# Initialize an empty list to store table metadata for all tables
+all_tables_metadata = []
 
-for col_name, col_type in columns:
-    col_type = col_type.strip()  # Clean up the column type string
-    table_metadata["columns"].append({
-        "column_name": col_name,
-        "column_type": col_type
-    })
+for create_stmt in create_statements:
+    table_name = create_stmt[0]
+    columns_section = create_stmt[1]
+    
+    columns = re.findall(r"\[?(\w+)\]?\s+(\w+\s*\(?[\w\s,]*\)?\s*\w*)[,)]", columns_section)
+    
+    # Construct the dictionary for each table
+    table_metadata = {
+        "table_name": table_name,
+        "columns": []
+    }
 
-# Serialize the dictionary to JSON and print
-json_output = json.dumps(table_metadata, indent=4)
-print(json_output)
+    for col_name, col_type in columns:
+        col_type = col_type.strip()  # Clean up the column type string
+        table_metadata["columns"].append({
+            "column_name": col_name,
+            "column_type": col_type
+        })
+
+    all_tables_metadata.append(table_metadata)
+
+# Serialize the list of dictionaries to JSON and save to a file
+with open('output.json', 'w') as json_file:
+    json.dump(all_tables_metadata, json_file, indent=4)
